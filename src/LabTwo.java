daa607624d05f841a50fabcc0801bd393c3a1541
SarahGG <guarino.sgg@gmail.com>
2017-04-12 12:17:41 -0400

    adding basic structure for lab-2

---
+++ src/LabTwo.java
@@ -0,0 +1,10 @@
+
+
+/**
+ * Created by Sarah Guarino on 4/11/2017.
+ */
+public class LabTwo {
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+}
